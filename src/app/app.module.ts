5e6bf4a74655d706673e9f1d0ac3ed0eef36071e
VUK <vishnuudai@gmail.com>
2020-04-18 02:39:43 +0530

    Assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
-  declarations: [ AppComponent, MenuComponent],
+  declarations: [ AppComponent, MenuComponent, DishdetailComponent],
   imports: [
     BrowserAnimationsModule,
     MatToolbarModule,
