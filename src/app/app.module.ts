97cd40098769ee2a5be1643c34227d4f62c07c7f
VUK <vishnuudai@gmail.com>
2020-04-18 00:51:49 +0530

    Components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
@@ -20,6 +23,9 @@ import { MenuComponent } from './menu/menu.component';
     FlexLayoutModule,
     BrowserModule,
     MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
     AppRoutingModule
   ],
   providers: [],
