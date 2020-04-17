8683105cb4de35f75e684d64bd4110855c36d2ec
VUK <vishnuudai@gmail.com>
2020-04-18 00:08:10 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
+import {MatListModule} from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, MenuComponent],
   imports: [
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
     BrowserModule,
+    MatListModule,
     AppRoutingModule
   ],
   providers: [],
