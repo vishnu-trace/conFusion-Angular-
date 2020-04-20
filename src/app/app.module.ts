6a54cf94a9cfda8819de3134849375c0fde2fd0a
VUK <vishnuudai@gmail.com>
2020-04-20 16:13:43 +0530

    header & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,13 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 @NgModule({
-  declarations: [ AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [ AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserAnimationsModule,
     MatToolbarModule,
@@ -29,7 +34,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatGridListModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
