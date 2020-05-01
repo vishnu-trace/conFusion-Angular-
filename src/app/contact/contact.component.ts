f9f7d888373708f8d838b450c24c2503d53885b3
VUK <vishnuudai@gmail.com>
2020-05-01 23:43:40 +0530

    Responsive Form part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Feedback, ContactType} from '../shared/feedback'; 
 
@@ -13,6 +13,8 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  @ViewChild('fform') feedbackFormDirective;
+
   constructor(private fb: FormBuilder) {
     this.createForm();
 
@@ -23,11 +25,11 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
-      agree: '',
+      firstname: ['',Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0,Validators.required],
+      email: ['',Validators.required],
+      agree: false,
       contacttype: 'None',
       message: ''
     });
@@ -36,7 +38,16 @@ export class ContactComponent implements OnInit {
   onSubmit(){
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email:'',
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedbackFormDirective.resetForm();
   }
 
 }
+ 
