f63e1a4ca69296ecedfa67ef7f526ea408cc632e
VUK <vishnuudai@gmail.com>
2020-05-02 20:16:56 +0530

    Promises part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Dish} from '../shared/dish';
 import {DISHES} from '../shared/dishes';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,14 +12,23 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]>{
-    return Promise.resolve(DISHES);
+    return new Promise( resolve=>{
+      //Simulate Server Latency With 2s delay
+      setTimeout(() => resolve(DISHES),2000)
+    });
   }
 
   getDish(id: string): Promise<Dish>{
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+    return new Promise(resolve =>{
+    //Simulate Server Latency With 2s delay
+    setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]),2000)
+    });
   }
 
   getFeaturedDish():Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+    return new Promise(resolve =>{
+      //Simulate Server Latency With 2s delay
+      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]),2000)
+      });
   }
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -10,14 +10,25 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]>{
-    return Promise.resolve(LEADERS);
+    return new Promise( resolve => {
+      //Simulate Server Delay
+      setTimeout(()=>resolve(LEADERS),2000);
+    });
   }
 
   getLeader(id: string): Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader)=> leader.id)[0]);
+    return new Promise(resolve => {
+      //Simulate Server Delay
+      setTimeout(()=>resolve(LEADERS.filter((leader)=> leader.id)[0]),2000);
+    });
+
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve => {
+      //Simulate Server Delay
+      setTimeout(()=>resolve(LEADERS.filter((leader) => leader.featured)[0]),2000);
+    });
+
   }
 }
